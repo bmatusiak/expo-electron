2b97aff343b5657d7fbf6897bda903d661ace8c3
bmatusiak <bmatusiak@gmail.com>
2026-01-13 09:21:27 -0500

    feat: add dmg maker to Electron packaging options

--- cli.js
+++ cli.js
@@ -934,6 +934,7 @@ async function pack(makeMakers) {
             makers: [
                 { name: '@electron-forge/maker-squirrel', config: {} },
                 { name: '@electron-forge/maker-zip', platforms: ['darwin', 'linux', 'win32'] },
+                { name: '@electron-forge/maker-dmg', config: {} },
                 { name: '@electron-forge/maker-deb', config: {} },
                 { name: '@electron-forge/maker-rpm', config: {} }
             ]
